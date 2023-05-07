c3b1efb36d817195b6e335110b1b46fd115e9162
Ehtisham <40536476+ehtisham6699@users.noreply.github.com>
2023-05-07 22:06:46 +0500

    Update index.js

--- index.js
+++ index.js
@@ -47,13 +47,13 @@ exports.processOrder = async (event) => {
   // Loop through the products in the order
   console.log("AAAAAAAAA", event);
   console.log("BBBBB", event.Records[0].body);
-  const processOrderMessage = JSON.parse(event.Records[0].body);
+  const processOrderMessage = JSON.parse(event.Records[0].body.event);
   console.log(processOrderMessage);
   const { customer, products, address } = processOrderMessage;
   const newOrder = await new Order({
     customer: customer,
     products: products.map((id) => {
-      return new mongoose.Types.ObjectId(id);
+      new mongoose.Types.ObjectId(id);
     }),
     address: address,
   }).save();
