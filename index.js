bedd5ad3330b32aecb46937e126aeaa9d355cec4
Ehtisham <40536476+ehtisham6699@users.noreply.github.com>
2023-05-07 22:26:05 +0500

    Update index.js

--- index.js
+++ index.js
@@ -47,7 +47,11 @@ exports.processOrder = async (event) => {
   // Loop through the products in the order
   console.log("AAAAAAAAA", event);
   console.log("BBBBB", event.Records[0].body);
-  const processOrderMessage = JSON.parse(event.Records[0].body);
+  const processOrderMessage = JSON.parse(
+    event.Records[0].body.event
+      ? event.Records[0].body.event
+      : event.Records[0].body
+  );
   console.log(processOrderMessage);
   const { customer, products, address } = processOrderMessage;
   const newOrder = await new Order({
