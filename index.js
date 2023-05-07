dbb299cab7bd814f86e6f3de50da3fb578f3f067
Ehtisham <40536476+ehtisham6699@users.noreply.github.com>
2023-05-07 14:23:18 +0500

    updated customers code

--- index.js
+++ index.js
@@ -5,44 +5,50 @@ const connectDB = require("./database");
 exports.createOrder = async (event, context) => {
   try {
     await connectDB();
-    const requestBody = JSON.parse(event.body);
 
-    const newOrder = await Order.create(requestBody);
+    // Extract customer id and products array from request body
+    const { customer, products } = JSON.parse(event.body);
+
+    // Create a new order object
+    const newOrder = new Order({
+      customer: customer,
+      products: products.map((productId) => ({ product: productId })),
+    });
+
+    // Save the new order
+    await newOrder.save();
+
+    // Process the order and update product info
     await processOrder(newOrder);
+
     return {
       statusCode: 200,
       body: JSON.stringify(newOrder),
     };
   } catch (err) {
-    console.error("Error creating Order:", err);
+    console.error("Error creating order:", err);
     return {
       statusCode: 500,
       body: err,
     };
   }
 };
+
+// Function to process an order and update product info
 async function processOrder(order) {
-  try {
-    await connectDB();
-    if (order.products.length > 0) {
-      await Product.updateMany(
-        { _id: { $in: order.products } },
-        { soldOut: true },
-        (err, res) => {
-          if (err) {
-            console.log(err);
-          } else {
-            console.log(`${res.nModified} products updated`);
-          }
-        }
-      ).exec();
-    }
-  } catch (err) {
-    console.error("Error processing Order:", err);
-    return {
-      statusCode: 500,
-      body: err,
-    };
+  // Loop through the products in the order
+  for (let i = 0; i < order.products.length; i++) {
+    const productId = order.products[i].product;
+
+    // Update the product quantity and sold out flag
+    await Product.findByIdAndUpdate(
+      productId,
+      {
+        $inc: { quantity: -1 },
+        $set: { soldOut: true },
+      },
+      { new: true }
+    );
   }
 }
 exports.getCustomerOrder = async (event, context) => {
@@ -50,15 +56,15 @@ exports.getCustomerOrder = async (event, context) => {
   try {
     await connectDB();
     // find all orders for a specific user
-    const orders = await Order.find({ customer: event.pathParameters._id });
-    console.log("Orders:", orders);
+    const orders = await Order.find({ customer: event.pathParameters._id })
+      .populate("customer")
+      .populate("products");
     return {
       statusCode: 200,
       body: JSON.stringify(orders),
     };
   } catch (err) {
     console.error("Error fetching  all orders:", err);
-    console.error("bodyyy:", JSON.parse(event));
     return {
       statusCode: 500,
       body: JSON.stringify(err),
